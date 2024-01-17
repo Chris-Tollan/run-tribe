f0fdd9af1c8ff684c48744afe3a2817d2e4b9cf9
Chris-Tollan <>
2024-01-17 21:58:02 +0000

    add meta class + str method to Post model

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | By {self.author} | On {self.created_on}"
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -26,3 +32,4 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+

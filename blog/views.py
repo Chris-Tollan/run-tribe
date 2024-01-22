699b00a603df1a8ed214b30dae438d4ed6a81582
Chris-Tollan <>
2024-01-22 22:38:17 +0000

    create html&blog template, create style.css

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
-    template_name = "post_list.html"
+    template_name = "blog/blog.html"
+    paginate_by = 4

daf2036e8c794dc63763c43f69cfbdfb38057df7
Chris-Tollan <>
2024-01-24 22:19:55 +0000

    wire up home URLs

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from .models import Home
 
 # Create your views here.
 
 
 def home(request):
     """ Homepage """
-    return render(request, 'index.html')
+    return render(
+        request,
+        "home/index.html",
+        {"home": home},
+    )

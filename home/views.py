c9039541d2e28b69bdafeb2e82cbde7757bb3b75
Chris-Tollan <>
2024-01-30 21:04:56 +0000

    import form assign&pass variable in views

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
+from .forms import ContactUs
 
 # Create your views here.
 
 
 def home(request):
+    contact_us = ContactUs()
     """ Homepage """
     return render(
         request,
         "home/index.html",
-        {"home": home},
+        {"home": home,
+         "contact_us": contact_us},
     )

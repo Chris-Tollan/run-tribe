e5b786d347047c3acc6fc40f0cbe1d084269c3b4
Chris-Tollan <>
2024-01-30 20:58:19 +0000

    add ContactUs class +meta class

--- home/forms.py
+++ home/forms.py
@@ -1,2 +1,8 @@
 from .models import ContactUs
 from django import forms
+
+
+class ContactUs(forms.ModelForm):
+    class Meta:
+        model = ContactUs
+        fields = ('name', 'email', 'message')

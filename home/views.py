5ab46952cd5fad6d2e83d7e9e09a910c9600cbfa
Chris-Tollan <>
2024-01-30 22:03:14 +0000

    add tags to display  msg for form submission

--- home/views.py
+++ home/views.py
@@ -6,11 +6,16 @@ from .forms import ContactUs
 
 
 def home(request):
+
     if request.method == "POST":
         contact_us = ContactUs(data=request.POST)
         if contact_us.is_valid():
             contact_us.save()
-            messages.add_message(request, messages.SUCCESS, "Thanks for getting in touch, we will be in contact with you soon.")
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thanks for getting in touch, we will be in contact soon.'
+            )
+
     contact_us = ContactUs()
     """ Homepage """
     return render(

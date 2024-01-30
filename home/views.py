b266843accfe4b9e6253323d4099c07b3e956aba
Chris-Tollan <>
2024-01-30 21:33:01 +0000

    wire up post handler for contactus form

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import ContactUs
 
 # Create your views here.
 
 
 def home(request):
+    if request.method == "POST":
+        contact_us = ContactUs(data=request.POST)
+        if contact_us.is_valid():
+            contact_us.save()
+            messages.add_message(request, messages.SUCCESS, "Thanks for getting in touch, we will be in contact with you soon.")
     contact_us = ContactUs()
     """ Homepage """
     return render(

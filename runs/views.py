fffd88a8ba94769304fdee85c8e0d4874dc87e8f
Chris-Tollan <>
2024-02-01 12:02:17 +0000

    create initial runs view

--- runs/views.py
+++ runs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import About
 
-# Create your views here.
+
+def run_list(request):
+    """
+    Renders the Runs page
+    """
+    runs = Runs.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "runs/runs.html",
+        {"runs": run},
+    )

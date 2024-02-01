34ccdf551a6765b6c344ba8fb4c2693055642923
Chris-Tollan <>
2024-02-01 11:14:52 +0000

    register runs model in admin

--- runs/admin.py
+++ runs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Runs
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+@admin.register(Runs)
+class RunsAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'updated_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'updated_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)

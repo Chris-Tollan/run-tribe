d4a2c661a983c61e1edb2940ae9ad1b8ef4a82f1
Chris-Tollan <>
2024-02-02 22:14:15 +0000

    create book_run view

--- runs/views.py
+++ runs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Runs
 
@@ -7,3 +7,14 @@ class RunList(generic.ListView):
     queryset = Runs.objects.filter(status=1)
     template_name = "runs_list.html"
     paginate_by = 6
+
+
+def book_run(request, slug):
+    queryset = Runs.objects.filter(status=1)
+    run = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "runs/book_run.html",
+        {"post": post},
+    )

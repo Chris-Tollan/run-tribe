fd67c93d79670a8b5aa1ae73bd717b28babe1f6a
Chris-Tollan <>
2024-02-07 23:11:14 +0000

    create RunList view

--- runs/views.py
+++ runs/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Runs
 
-# Create your views here.
+
+class RunList(generic.ListView):
+    queryset = Runs.objects.filter(status=1)
+    template_name = "runs_list.html"
+    paginate_by = 6

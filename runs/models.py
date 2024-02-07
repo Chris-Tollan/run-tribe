696df63aed86a763d943ba612f509238b688c5fc
Chris-Tollan <>
2024-02-07 23:04:47 +0000

    register runs app in admin

--- runs/models.py
+++ runs/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Runs(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField()
+    excerpt = models.TextField(blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Runs'
+
+    def __str__(self):
+        return f"Run - {self.title}"

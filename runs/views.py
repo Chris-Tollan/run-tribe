5bc9f153c0755841d6f78c6e5d0451f627bfab38
Chris-Tollan <>
2024-02-08 11:48:19 +0000

    create RunInformation view

--- runs/views.py
+++ runs/views.py
@@ -8,3 +8,15 @@ class RunList(generic.ListView):
     queryset = Runs.objects.filter(status=1)
     template_name = "runs_list.html"
     paginate_by = 6
+
+
+class RunInformation(View):
+    def get(self, request, slug, *args, **kwargs):
+        run_detail = get_object_or_404(Runs, slug=slug, status=1)
+        available_runs = AvailableRuns.objects.filter(
+            runs=run_detail).order_by('updated_on')
+
+        return render(request, 'book_run.html', {
+            'run_detail': run_detail,
+            'available_runs': available_runs
+        })

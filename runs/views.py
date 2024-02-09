0a570ff91019e9d7fdf6608c9d4024bcdf8c3265
Chris-Tollan <>
2024-02-09 17:55:41 +0000

    amend view to submit run in bookingform

--- runs/views.py
+++ runs/views.py
@@ -22,7 +22,7 @@ class RunDetail(View):
 
         return render(
             request,
-            "run_detail.html",
+            "runs/book_run.html",
             {
                 "run": run,
                 "booking_form": BookingForm()
@@ -39,14 +39,19 @@ class RunDetail(View):
         if booking_form.is_valid():
 
             # assign the booking user to the requesting user
-            booking_form.instance.user = request.user.username
+            booking_form.instance.user = request.user
             # do a false commit of the form to store the data
             booking = booking_form.save(commit=False)
             # assign the booking title to the current run
-            booking_form.title = run
+            booking_form.instance.title = run
             # save the form
             booking_form.save()
             # redirect the user
-            return HttpResponseRedirect(reverse('bookings', args=[request.user]))
-        else:
-            booking_form = BookingForm()
+        return render(
+            request,
+            "runs/book_run.html",
+            {
+                "run": run,
+                "booking_form": booking_form
+            },
+        )

a20b3b099a6cc9000c79a71ba8439059b01b6901
Chris Tollan <chrisytollan@googlemail.com>
2024-02-18 21:23:47 +0000

    rename page and create view for updating user details

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import ContactUs
+from .forms import ContactUs, UserUpdate
 
 # Create your views here.
 
@@ -23,4 +23,32 @@ def home(request):
         "home/index.html",
         {"home": home,
          "contact_us": contact_us},
+    )
+
+
+from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContactUs
+
+# Create your views here.
+
+
+def update_user_details(request):
+
+    if request.method == "POST":
+        user_update = UserUpdate(data=request.POST)
+        if user_update.is_valid():
+            user_update.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thanks, we will contact you on the email address provided within 24 hours.'
+            )
+
+    user_update = UserUpdate()
+    """ User Update Page """
+    return render(
+        request,
+        "home/update_user_details.html",
+        {"update_user_details": update_user_details,
+         "user_update": user_update},
     )

58b121562581c1bd0a0b27c4c061c6502d5dcbbe
Chris Tollan <chrisytollan@googlemail.com>
2024-02-18 22:08:01 +0000

    create view to update users details

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+
+def update_user_details(request):
+
+    if request.method == "POST":
+        user_update = UserUpdate(data=request.POST)
+        if user_update.is_valid():
+            user_update.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thanks, we will contact you on the email address provided within 24 hours.'
+            )
+
+    user_update = UserUpdate()
+    """ User Update Page """
+    return render(
+        request,
+        "home/update_user_details.html",
+        {"update_user_details": update_user_details,
+         "user_update": user_update},
+    )
+

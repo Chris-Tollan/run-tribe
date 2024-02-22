3a082fe4b649db414068c60962203715db9b405d
Chris Tollan <chrisytollan@googlemail.com>
2024-02-22 23:26:58 +0000

    make runs urls PEP8 compliant

--- runs/urls.py
+++ runs/urls.py
@@ -5,7 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.RunList.as_view(), name='runs'),
     path('<slug:slug>/', views.RunDetail.as_view(), name='run_detail'),
-    path('runs/my-bookings/', views.MyBookings.as_view(), name='my_bookings'),
-    path('delete/<int:pk>/', views.BookingDeleteView.as_view(), name='delete_booking'),
-    path('update/<int:pk>/', views.BookingUpdateView.as_view(), name='update_booking')
+    path('runs/my-bookings/', views.MyBookings.as_view(), 
+         name='my_bookings'),
+    path('delete/<int:pk>/', views.BookingDeleteView.as_view(), 
+         name='delete_booking'),
+    path('update/<int:pk>/', views.BookingUpdateView.as_view(), 
+         name='update_booking')
 ]
